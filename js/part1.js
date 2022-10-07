868e3f3d304ca684c8b624344df06c6d7ec68c7f
Roman Kutselepa <roman.kutselepa@gmail.com>
2022-10-07 17:47:12 +0300

    first commit

---
+++ js/part1.js
@@ -0,0 +1,92 @@
+// let userAge;
+
+// userAge = 50;
+// console.log(userAge);
+
+// userAge = 'HELLO WORLD';
+// console.log(userAge);
+
+// const userName = 'Jack';
+// console.log(userName);
+
+// let x = -Infinity;
+// console.log(typeof x);
+// let pi = 3.14;
+// console.log(typeof pi);
+// let exp = 123e5;
+// console.log(typeof exp);
+// console.log(exp);
+
+// arithmetic operators
+// +, -, /, *, **, %
+
+// let x = 10;
+// let y = (x + 10 - 5) % 2 ** 0.5;
+// console.log(y);
+// let x = 1.500000;
+// console.log(typeof x.toFixed(0));
+// console.log(x);
+// x = x.toFixed(10);
+// console.log(typeof x);
+// x = x + 2
+// console.log(x);
+
+
+// let userName = "Roman";
+// let userSurname = "Kutselepa";
+
+// console.log("Hello " + userName + " " + userSurname + "!");
+
+// let message = "That's it";
+// let message2 = 'My favourite book is "METRO 2033"';
+// let message3 = '"Metro 2033" that\'s my favourite book';
+// let message4 = "My favourite book is \"METRO 2033\"";
+// let message5 = 'symbol \\ is used as escape character';
+// console.log(message5);
+
+// let greet = '123123123';
+// console.log(greet.length);
+// console.log(greet);
+
+// let a = true;
+// let b = false;
+
+// let c = null;
+// let d = undefined;
+// let e = 10 - 'Hello';
+// console.log(e);
+// console.log(Boolean(0));
+// console.log(Boolean(-0));
+// console.log(Boolean(""));
+// console.log(Boolean(null));
+// console.log(Boolean(undefined));
+// console.log(Boolean(NaN));
+
+// let greeting = 'Hello world!';
+//               //01234567891011
+// console.log(greeting.length);
+// console.log(greeting[greeting.length-1]);
+
+// let a = greeting.slice(undefined, 5);
+// let name = 'Roman';
+// let result = a + ' ' + name;
+// console.log(result);
+
+// console.log(greeting.slice(-6, -1))
+
+let greeting = 'Hello world!';
+let greeting2 = 'HELLO WORLD!';
+
+console.log(greeting.toUpperCase());
+console.log(greeting.toLowerCase());
+
+console.log(greeting.toUpperCase() === greeting2);
+
+let x = 'John'
+let message = `Hello ${x.toUpperCase()}! Yo'ur "a"ge is ${2022 - 1988}.`;
+
+console.log(message);
+
+
+let y = '1.Do\n2.Do that\n3.Do something\n4.Exit';
+console.log(y);
